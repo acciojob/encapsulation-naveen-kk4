bfb1355a1f0a630ee3dd900147d7b925c55d6d6b
naveen-kk4 <naveennig2001@gmail.com>
2023-05-10 15:09:45 +0530

    changes updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly a = new RWOnly();
+        // a.name = "Naveen";
+        // name has private access in RWOnly
+        a.setName("Naveen");
+        System.out.println(a.getName());
+    }
   
 }
